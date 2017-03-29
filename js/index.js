5e210f46a752b183251129d2569257087deb544f
Lucas Russell Kern <lucaskern8@gmail.com>
2017-03-29 17:06:13 -0400

    Added content to work sections. Style changes, Added JqueryUI link, image and content changes

--- js/index.js
+++ js/index.js
@@ -1,27 +1,31 @@
 $(document).ready(function () {
   console.log("index.js ran");
 
-  $("#web, #interact, #fun").hide();
+  $("#web, #software, #fun").hide();
 
 
   $(".webBtn").click(function () {
 
-    $("#interact, #fun").slideUp("slow");
+    $("#software, #fun").slideUp("slow");
 
     $("#web").slideToggle("slow");
   });
   
   $(".funBtn").click(function () {
 
-    $("#web, #interact").slideUp("slow");
+    $("#web, #software").slideUp("slow");
 
     $("#fun").slideToggle("slow");
   });
   
-  $(".interactBtn").click(function () {
+  $(".softwareBtn").click(function () {
 
     $("#web, #fun").slideUp("slow");
 
-    $("#interact").slideToggle("slow");
+    $("#software").slideToggle("slow");
   });
+  
+  $( function() {
+    $( "#tabs" ).tabs();
+  } );
 });
